959ca18e998c77d9d5d215a3fccfd64f07de5425
MrGoose <sivyakov.pavel@bk.ru>
2025-10-23 16:35:38 +0300

    Add files via upload

---
+++ 4задание.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int userNumber;
+    int userAnswer;
+    cout << "[ + ] Enter numbeer: ";
+    cin >> userNumber;
+    for (int programMultiplier = 1; programMultiplier <= 9; programMultiplier++) {
+        cout << "[ + ] Answer to the quastion: " << userNumber << " x " << programMultiplier << endl;
+        cout << "[ + ] Answer: ";
+        cin >> userAnswer;
+        if (userAnswer == userNumber * programMultiplier) {
+            cout << "[ + ] The example was solved correctly!" << endl;
+        }
+        else {
+            cout << "[ - ] Error, the example was solved incorrectly!" << endl;
+            return 0; 
+        }
+    }
+    cout << "[ + ] The examples are solved, congratulations!" << endl;
+    return 0;
+}
