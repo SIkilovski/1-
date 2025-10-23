bc8197c4cbfaacd6e86ca5d790a68f096253ce74
MrGoose <sivyakov.pavel@bk.ru>
2025-10-23 16:03:58 +0300

    Update and rename FileName1.cpp to Задание 1.cpp

---
+++ Задание 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int alpha = 12;
+	cout << "alpha = " << alpha << " | òèï: int, ðàçìåð: 4 á." << endl;
+	short delta = 9;
+	cout << "delta = " << delta << " | òèï: short, ðàçìåð: 2 á." << endl;
+	float grey = 1.6;
+	cout << "grey = " << grey << " | òèï: float, ðàçìåð: 4 á." << endl;
+	double red = 1.29;
+	cout << "red = " << red << " | òèï: double, ðàçìåð: 8 á." << endl;
+	char krey = 'a';
+	cout << "krey = " << krey << " | òèï: char, ðàçìåð: 1 á." << endl;
+}
