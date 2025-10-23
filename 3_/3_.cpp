81bddc534aa2247da88941ec05012358e883793d
MrGoose <sivyakov.pavel@bk.ru>
2025-10-23 15:59:27 +0300

    Add files via upload

---
+++ 3_/3_.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "[+] Enter a number: ";
+    cin >> number;
+    if (number < 0) {
+        cout << "[+] Mistake! A number less than 0." << endl;
+    }
+    else if (number > 100) {
+        cout << "[+] Error! The number is greater than 100." << endl;
+    }
+    else {
+        int lower_bound = (number / 10) * 10 + 1;
+        int upper_bound = (number / 10 + 1) * 10;
+        if (number <= 10) {
+            lower_bound = 0;
+            upper_bound = 10;
+        }
+        cout << "[+] a number in the range: " << lower_bound << " - " << upper_bound << endl;
+    }
+    return 0;
+}
