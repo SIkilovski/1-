cd7a075849f49759d5bde03b39c3593e18b0bcb7
MrGoose <sivyakov.pavel@bk.ru>
2025-12-11 13:23:44 +0300

    Практическая работа W5

---
+++ W5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>  
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "+ Введите число: ";
+    cin >> number;
+ 
+    int counter = 1;
+
+    while (counter <= 10){
+        cout << "+ " << number << " × " << counter << " = " << number * counter << endl;
+        counter++; 
+    }
+    return 0;
+}
