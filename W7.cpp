3d5c31aed057742a370201f897b8a0e3c29da9f0
MrGoose <sivyakov.pavel@bk.ru>
2025-12-12 11:17:14 +0300

    Практическая работа W7

---
+++ W7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    string password;
+    const string correctPassword = "12345";
+
+    do {
+        cout << "[+] Введите пароль: ";
+        cin >> password;
+
+        if (password != correctPassword) {
+            cout << "[-] Неверный пароль!" << endl;
+        }
+    } while (password != correctPassword);
+
+    cout << "[+] Добро пожаловать" << endl;
+
+    return 0;
+}
