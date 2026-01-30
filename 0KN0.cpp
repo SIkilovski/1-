48dc3a961981a73e291a27cd1392ad5424d089a8
MrGoose <sivyakov.pavel@bk.ru>
2026-01-30 12:46:52 +0300

    Окно

---
+++ 0KN0.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int size;
+
+    cout << "Введите размер: ";
+    cin >> size;
+
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (row == 0 || row == size - 1 || col == 0 || col == size - 1) {
+                cout << "* ";
+            }
+            else if (row == size / 2) {
+                cout << "- ";
+            }
+            else if (col == size / 2) {
+                cout << "| ";
+            }
+            else {
+                if (row < size / 2 && col < size / 2) {
+                    cout << "# "; 
+                }
+                else {
+                    cout << "  "; 
+                }
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
