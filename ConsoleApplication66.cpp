8a2209a62ac288a77f41afcec238ae1f98249a46
MrGoose <sivyakov.pavel@bk.ru>
2026-03-26 14:59:37 +0300

    матем
    как оно работает
    Я так сделал и оно работает, каким #### оно работает тебя ##### не должно

---
+++ ConsoleApplication66.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int choice;
+	int num;
+	int userAnswer;
+	setlocale(0, "");
+	cout << "Математический чемпионат" << endl;
+	cout << "[1]Начать игру\n";
+	cout << "[2]Выйти из игры\n";
+	cout << "Введите число: ";
+	cin >> choice;
+	if (choice == 1) {
+		cout << "игра началась" << endl;
+	}
+	while (true) {
+		int a = rand() % 100 + 1;
+		int b = rand() % 100 + 1;
+		cout << "\n" << a << "+" << b << "=" << endl;
+		if (choice == 2) break;
+		cin >> userAnswer;
+
+		if (userAnswer == a + b) {
+			cout << "Правильно";
+		}
+		else {
+			cout << "Неверно (ответ" << a + b << "). ";
+			
+
+
+
+
+
+
+			return 0;
+		}
+	}
+}
